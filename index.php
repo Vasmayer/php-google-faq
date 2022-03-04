085dc711b4fb480c0ceaabee70aa2798f4061224
Vasmayer <lealimarco90@gmail.com>
2022-03-04 16:37:51 +0100

    array structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Riscrivere questa pagina del sito google:
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Realizziamo la struttura dati che ci sembra più adatta per poter 
+poi stampare con un ciclo le domande e risposte della sezione centrale.
+Consigli:
+Teniamo a mente le differenze tra array associativo ed array con indici numerici
+Ricordiamo che possiamo inserire tag all'interno delle stringhe di PHP, saranno convertiti in HTML quando faremo echo
+Concentriamoci sulle FAQ e lasciamo per ultimo header e footer (e il loro CSS)
+Lasciamo per ultimo header e footer (e il loro CSS) e concentriamoci sulle FAQ
+Prima facciamo le FAQ e solo dopo header e footer.
+BONUS:
+Inserire le voci di menu dell'header tramite un array di PHP (i link non devono funzionare) */
+
